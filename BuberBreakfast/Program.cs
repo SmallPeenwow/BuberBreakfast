8370736e5b5bb32bc6128993aa1520711951fd04
SmallPeenwow <bowdencamryn@gmail.com>
2023-07-17 19:53:01 +0200

    Create Service Interface Done

--- BuberBreakfast/Program.cs
+++ BuberBreakfast/Program.cs
@@ -1,8 +1,11 @@
+using BuberBreakfast.Services.Breakfasts;
+
 var builder = WebApplication.CreateBuilder(args);
 {
     // Add services to the container.
 
     builder.Services.AddControllers();
+    builder.Services.AddScoped<IBreakfastService, BreakfastService>();
 }
 
 var app = builder.Build();
